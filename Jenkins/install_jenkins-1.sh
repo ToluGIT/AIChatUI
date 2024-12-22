5c05430053ef92dfb5c6f6c922b8ea7480c649d2
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-12-22 08:37:47 +0100

    Create install_jenkins-1.sh

---
+++ Jenkins/install_jenkins-1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+sudo yum update -y  
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo  
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  
+sudo yum upgrade -y 
+sudo dnf install java-17-amazon-corretto -y  
+sudo yum install jenkins -y  
+sudo systemctl enable jenkins  
+sudo systemctl start jenkins
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo usermod -aG docker jenkins
+sudo yum install -y git
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh  -s -- -b /usr/local/bin v0.18.3
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:lts-community
