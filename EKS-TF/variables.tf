d65793370c656ea12b5d1c7d25af8f52a6aff5b0
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-12-22 10:32:59 +0100

    Update variables.tf

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -5,10 +5,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "aws_profile" {
-  description = "AWS profile"
-  type        = string
-}
 
 variable "project_name" {
   description = "Project name"
