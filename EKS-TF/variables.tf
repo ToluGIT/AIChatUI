40ed20227949631f4a7d144817d73aecd3eeac87
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-12-22 10:22:50 +0100

    Update variables.tf

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -1,12 +1,30 @@
-variable "vpc-name" {}
-variable "igw-name" {}
-variable "rt-name2" {}
-variable "subnet-name" {}
-variable "subnet-name2" {}
-variable "security-group-name" {}
-variable "iam-role-eks" {}
-variable "iam-role-node" {}
-variable "iam-policy-eks" {}
-variable "iam-policy-node" {}
-variable "cluster-name" {}
-variable "eksnode-group-name" {}
+# variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS profile"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "aichatui"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.31"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
