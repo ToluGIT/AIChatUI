6c79c07691fa2c805be543407c67d6b16b644cd4
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-12-23 11:33:43 +0100

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,16 +1,19 @@
+# backend.tf
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-states-ai-chat-ui"
-    region         = "us-east-1"
-    key            = "EKS/terraform.tfstate"
-    dynamodb_table = "terraform-locks-ai-chat-ui"
-    encrypt        = true
-  }
+  backend "s3" {}
+}
+
+# provider.tf
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
       source  = "hashicorp/aws"
+      version = ">= 2.7.0"
     }
   }
 }
