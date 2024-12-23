88715bb5e2d276d30523c9ca5dde4a67ccd95a3a
ToluGIT <93225033+ToluGIT@users.noreply.github.com>
2024-12-23 11:37:31 +0100

    Update provider.tf

--- EKS-TF/provider.tf
+++ EKS-TF/provider.tf
@@ -1,3 +1,14 @@
+# provider.tf
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
+}
+
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+  }
 }
